d5452adafd9a05574ab75c4ef53fdc212d5e762a
Wesley-Martinez14 <riwes1403@gmail.com>
2024-04-15 23:02:50 -0400

    Parcial 3

---
+++ .venv/test_app.py
@@ -0,0 +1,13 @@
+# test_app.py
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_hello_world(client):
+    response = client.get('/')
+    assert response.data == b'¡Hola,soy Wesley Martinez!'
